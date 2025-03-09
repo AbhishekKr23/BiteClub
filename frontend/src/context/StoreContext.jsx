39abc460cc5525675e5e747a55fb79fa94490665
Abhishek Kumar <107882573+AbhishekKr23@users.noreply.github.com>
2025-03-09 22:09:24 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
 const [cartItems,setCartItems] = useState({});
-const url = "https://biteclub-backend.vercel.app/"
+const url = "https://biteclubbackend.netlify.app/"
 const [token,setToken] = useState("")
 const [food_list,setFoodList] = useState([])
 
